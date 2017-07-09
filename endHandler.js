688f2001cfc7a3bec4741cbc6c0cc411e72de028
rebstan97 <rebecca_tan_97@hotmail.com>
2017-07-09 11:12:33 +0800

    More updated version of endHandler

---
+++ endHandler.js
@@ -0,0 +1,93 @@
+const Telegraf = require('telegraf');
+const { Extra, Markup } = require('telegraf');
+const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
+const Route = require('../models/route');
+const rankHandler = require('../handlers/rankHandler');
+
+module.exports = function(bot){
+
+    bot.action(/end(.+)/, function(ctx) {
+
+        var source = ctx.callbackQuery.data.split(':')[2];
+        var destination = ctx.callbackQuery.data.split(':')[1];
+        var routes = new Array();
+        //var busesToTake = new Array();
+
+        if (source === destination){
+            return ctx.reply('Are you trying to trick me? That\'s not funny');
+        };
+
+        MongoClient.connect('mongodb://rebstan97:orbitalbus@ds151232.mlab.com:51232/orbitalbot', function(err, db) {
+
+            if (err) throw err;
+
+            db.collection("busstops").findOne({name: source}).then(function(result) {
+
+                result.buses.forEach(function(bus){
+
+                    var indexSource = bus.busStops.indexOf(source);
+                    console.log(indexSource);
+                    var indexDestination = bus.busStops.indexOf(destination);
+                    console.log(indexDestination);
+
+                    if (indexDestination !== -1 && indexSource < indexDestination){
+
+                        var numStops = indexDestination - indexSource;
+                        //insert arrival time function after its done
+                        var waitingTime = Math.floor((Math.random() * 10) + 1);
+                        var travelTime = waitingTime + 2*numStops;
+
+                        routes.push(new Route({
+                            bus: bus.name,
+                            message: `Take bus ${bus.name} to ${destination}, reaching in 15 minutes`,
+                            waitingTime: waitingTime,
+                            travelTime: travelTime
+                        })); 
+             
+                    }
+
+                });
+
+                var newRoutes = rankHandler(routes);
+
+                if (newRoutes.length === 0){
+                    ctx.reply('No direct buses available!');
+
+                } else {
+
+                    var possibleRoutes = "";
+                    var rank = 1;
+                    var shortestWait = 20;
+
+                    newRoutes.forEach(function(route) {
+
+                        if(rank === 1) { shortestWait = route.waitingTime; }
+        
+                        possibleRoutes += `${rank}. Take bus ${route.bus} to ${destination}, reaching in ${route.waitingTime} minutes. Total travelling time is ${route.travelTime} minutes!\n`;
+                        rank++;
+
+                    });
+
+                    ctx.reply(possibleRoutes);
+
+                    db.collection("busstops").findOne({name: source}).then(function(result) {
+
+                        if (shortestWait >= 6) {
+                            ctx.reply('The waiting time is long! \n' + result.description);
+                        } else {
+                            ctx.reply('If you\'re not already there please hurry! The bus is coming real soon... ');
+                        }
+                        
+                    });
+
+                }
+
+                db.close();
+            });
+
+        });
+            
+    });
+
+};
