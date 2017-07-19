c48dd7a04115e6da590d7e1b09e8fa99947e53bb
rebstan97 <rebecca_tan_97@hotmail.com>
2017-07-19 23:36:03 +0800

    Consolidating code

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bot = new Telegraf('377118340:AAEUEccw3pbt4FsXJEhbCmXrCDKn-Too2AA');
 
 bot.use(Telegraf.log());
 
+//Invalid user input
+bot.hears(/^(?!\/start)(?!\/customise)(.+)$/, function(ctx) {
+    return ctx.reply('Enter /start to begin!');
+})
+
 //Call '/start' command
 const startBot = require('./commands/startCommand');
 startBot(bot);
@@ -12,5 +17,30 @@ startBot(bot);
 //Handle '/start' command
 const startHandler = require('./handlers/startHandler');
 startHandler(bot);
+
+const nearestStopHandler = require('./handlers/nearestStopHandler');
+nearestStopHandler(bot);
+
+bot.command('customise', function(ctx) {
+    ctx.reply('Add bus stops to your favourites list!', 
+        Markup.inlineKeyboard([
+            [Markup.callbackButton('AS7', 'add:AS7')],
+            [Markup.callbackButton('BIZ2', 'add:BIZ2')],
+            [Markup.callbackButton('Central Library', 'add:Central Library')]
+        ]).extra()
+    );
+});
+
+bot.action(/add(.+)/, function(ctx) {
+
+    var favStop = ctx.callbackQuery.data.split(':')[1];
+    console.log(favStop);
+    return ctx.reply(`Bus stop ${favStop} added!`, 
+        Markup.keyboard([`${favStop}`])
+        .resize()
+        .extra()
+        
+    );
+});
 
 bot.startPolling();
