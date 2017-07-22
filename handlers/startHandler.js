479ef89d161411b8c24920773275975b49389dfd
CindyTsai1 <cindy1.tsai@gmail.com>
2017-07-22 12:29:21 +0800

    Update startHandler.js

--- handlers/startHandler.js
+++ handlers/startHandler.js
@@ -17,16 +17,24 @@ module.exports = function(bot){
             db.collection("busstops").findOne({name: source}).then(function(result) {
                 
                 //getting the current unix timestamp of the message and convert to hours
-                var hour = (ctx.message.date)/60/60%24;
+                //var hour = (ctx.message.date)/60/60%24;
                 //ctx.reply(hour); //see if it prints out the current time for hour
                 
                 result.buses.forEach(function(bus) {
-                    if(bus.startTime <= hour && hour <= bus.endTime){
+                    var arrivalTime = Math.floor((Math.random() * 10) + 1);
+                    //if not operating, the api shall not return any value
+                    if(arrivalTime == null){
+                        arrivalResponse += `${bus.name} is not operating now\n`;
+                    }else{
+                        arrivalResponse += `${bus.name2}         ${arrivalTime} min\n`;
+                    };
+                    
+                    /*if(bus.startTime <= hour && hour <= bus.endTime){
                         var arrivalTime = Math.floor((Math.random() * 10) + 1);
                         arrivalResponse += `${bus.name2}         ${arrivalTime} min\n`;
                     }else{
                         arrivalResponse += `${bus.name} is not operating now\n`;
-                    };
+                    };*/
                 });
                 
                 ctx.reply(arrivalResponse, Markup.inlineKeyboard([
