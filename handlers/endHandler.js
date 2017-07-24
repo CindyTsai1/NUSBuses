5c6c7572d165ebee6079981cfef7ec8473439374
CindyTsai1 <cindy1.tsai@gmail.com>
2017-07-24 12:02:00 +0800

    Update endHandler.js

--- handlers/endHandler.js
+++ handlers/endHandler.js
@@ -29,52 +29,14 @@ module.exports = function(bot){
 
                 result.buses.forEach(function(bus){
                      
-                    //if destination not found, search the opposite route;
-                    if (bus.busStops.indexOf(destination) === -1){
-                        source = source.oppBusStops;
-                        bus = bus.oppBus;
-                    };
-                    
-                    var indexSource = bus.busStops.indexOf(source);
-                    var indexDestination = bus.busStops.indexOf(destination);
-
-                    //insert arrival time function after its done
-                    var waitingTime = Math.floor((Math.random() * 10) + 1);
-
-                    var numStops = indexDestination - indexSource;
-                    
-                    //if indexDestination is before indexSource and the starting terminal is the same as the ending terminal, check the number of stops if the bus reach destination and continue
-                    if(numStops < 0 && bus.name === "A1"){
-                        numStops = bus.busStops.length + numStops;
-                        //waitingTime2 is for the A1 at the terminal
-                        var waitingTime2 = Math.floor((Math.random() * 10) + 1);
-                        var travelTime = waitingTime + waitingTime2 + 2*numStops;
+                    //if destination is found, search it as normal;
+                    if (bus.busStops.indexOf(destination) !== -1){
+                        var indexSource = bus.busStops.indexOf(source);
+                        var indexDestination = bus.busStops.indexOf(destination);
+                        routes = findRouteHandler(routes, indexSource, indexDestination);
+                    }
+                    //should we find opposite bus stops
                         
-                        routes.push(new Route({
-                            bus: bus.name,
-                            message: `Take bus A1 to ${bus.busStops[0].name}, reaching in ${waitingTime} minutes, and take the same bus again, reaching in ${waitingTime2}.`,
-                            travelTime: travelTime
-                        }));
-                        
-                        //waitingTime2 for D2 at the terminal
-                        waitingTime2 = Math.floor((Math.random() * 10) + 1);
-                        travelTime = waitingTime + waitingTime2 + 2*numStops;
-                        
-                        routes.push(new Route({
-                            bus: bus.name,
-                            message: `Take bus A1 to ${bus.busStops[0].name}, reaching in ${waitingTime} minutes, and take bus D2, reaching in ${waitingTime2}.`,
-                            travelTime: travelTime
-                        }));
-                        
-                    } else if(numStops > 0) {
-                        travelTime = waitingTime + 2*numStops;
-                        
-                        routes.push(new Route({
-                            bus: bus.name,
-                            message: `Take bus ${bus.name} to ${destination}, reaching in ${waitingTime} minutes.`,
-                            travelTime: travelTime
-                        }));
-                    };
                 });
 
                 var newRoutes = rankRouteHandler(routes);
@@ -86,12 +48,12 @@ module.exports = function(bot){
 
                     var possibleRoutes = "";
                     var rank = 1;
-                    var shortestWait = newRoutes[0].travelTime;
+                    var shortestWait = newRoutes[0].waitingTime;
 
                     newRoutes.forEach(function(route) {
 
-                        if (rank === 1) { shortestWait = route.waitingTime; }
-                        if (rank == 2) { possibleRoutes += '\nAlternative route(s):\n'; }
+                        //if (rank === 1) { shortestWait = route.waitingTime; }
+                        if (rank === 2) { possibleRoutes += '\nAlternative route(s):\n'; }
                         
                         possibleRoutes += `${route.message} Total time needed to get to ${destination} is ${route.travelTime} minutes!\n`;
                         rank++;
