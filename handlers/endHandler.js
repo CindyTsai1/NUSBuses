e478b7232e035d61b0c9346d0ae2418c783ac253
CindyTsai1 <cindy1.tsai@gmail.com>
2017-07-24 17:29:28 +0800

    Update endHandler.js

--- handlers/endHandler.js
+++ handlers/endHandler.js
@@ -1,96 +1,3 @@
-/*
-const Telegraf = require('telegraf');
-const { Extra, Markup } = require('telegraf');
-const http = require('http');
-const MongoClient = require('mongodb').MongoClient;
-const Route = require('../models/route');
-const routeHandler = require('./routeHandler');
-const rankRouteHandler = require('./rankRouteHandler');
-
-module.exports = function(bot){
-
-    bot.action(/end(.+)/, function(ctx) {
-
-        var source = ctx.callbackQuery.data.split(':')[2];
-        var destination = ctx.callbackQuery.data.split(':')[1];
-        var routes = new Array();
-
-        if (source === destination){
-            return ctx.reply('Are you trying to trick me? That\'s not funny :( ', 
-                Markup.inlineKeyboard([
-                    Markup.callbackButton('I\'m sorry! Let me select my destination again', `route:${source}`)
-                ]).extra());
-        };
-
-        MongoClient.connect('mongodb://rebstan97:orbitalbus@ds151232.mlab.com:51232/orbitalbot', function(err, db) {
-
-            if (err) throw err;
-
-            db.collection("busstops").findOne({name: source}).then(function(result) {
-
-                result.buses.forEach(function(bus){
-
-                    var indexSource = bus.busStops.indexOf(source);
-                    var indexDestination = bus.busStops.indexOf(destination);
-
-                    if (indexDestination !== -1 && indexSource < indexDestination){
-
-                        var numStops = indexDestination - indexSource;
-                        //insert arrival time function after its done
-                        var waitingTime = Math.floor((Math.random() * 10) + 1);
-                        var travelTime = waitingTime + 2*numStops;
-
-                        routes.push(new Route({
-                            bus: bus.name,
-                            waitingTime: waitingTime,
-                            travelTime: travelTime
-                        })); 
-             
-                    }
-
-                });
-
-                var newRoutes = rankRouteHandler(routes);
-
-                if (newRoutes.length === 0) {
-                    ctx.reply('No direct buses available!');
-
-                } else {
-
-                    var possibleRoutes = "";
-                    var rank = 1;
-                    var shortestWait = 20;
-
-                    newRoutes.forEach(function(route) {
-
-                        if (rank === 1) { shortestWait = route.waitingTime; }
-                        if (rank == 2) { possibleRoutes += '\nAlternative route(s):\n'; }
-                        
-                        possibleRoutes += `Take bus ${route.bus} to ${destination}, reaching in ${route.waitingTime} minutes. Total time needed to get to ${destination} is ${route.travelTime} minutes!\n`;
-                        rank++;
-
-                    });
-
-                    ctx.reply(possibleRoutes);
-
-                    if (shortestWait >= 6) {
-                        ctx.reply('The waiting time is long! \n' + result.description);
-                    } else {
-                        ctx.reply('If you\'re not already there please hurry! The bus is coming real soon... ');
-                    }
-
-                }
-
-                db.close();
-            });
-
-        });
-            
-    });
-
-};
-*/
-
 const Telegraf = require('telegraf');
 const { Extra, Markup } = require('telegraf');
 const http = require('http');
@@ -188,7 +95,7 @@ module.exports = function(bot){
                             //if destination is found, search it as normal;
                             if (indexDestination !== -1){
 
-                                var indexSource = bus.busStops.indexOf(source);
+                                var indexSource = bus.busStops.indexOf(oppSource);
                                 var numStops = indexDestination - indexSource;
         
                                 //insert arrival time function after its done
