8a8e6493c475a372862fae69098ec699a4547e81
rebstan97 <rebecca_tan_97@hotmail.com>
2017-07-26 17:19:46 +0800

    Update startHandler.js

--- handlers/startHandler.js
+++ handlers/startHandler.js
@@ -10,30 +10,31 @@ module.exports = function(bot){
         var source = ctx.callbackQuery.data.split(':')[1];
         var arrivalResponse = `Bus arrival timings at ${source} are:\n`;
 
+        var date = new Date();
+        var time = date.getHours()*100 + date.getMinutes();
+
         MongoClient.connect('mongodb://rebstan97:orbitalbus@ds151232.mlab.com:51232/orbitalbot', function(err, db) {
             
             if (err) throw err;
 
             db.collection("busstops").findOne({name: source}).then(function(result) {
-                
-                result.buses.forEach(function(bus) {
-                    
-                    var arrivalTime = Math.floor((Math.random() * 10) + 1);
-                    
-                    //if not operating, the api shall not return any value
-                    if(arrivalTime == null){
-                        arrivalResponse += `${bus.name} is not operating now\n`;
-                    }else{
+
+                for (var bus of result.buses) {
+
+                    if ((bus.name === 'A1E' && (time < 630 || time > 1000)) || 
+                       (bus.name === 'A2E' && (time < 1730 || time > 1900)) ||
+                       (bus.name === 'C' && (time < 1000))) {
+                            arrivalResponse += `${bus.name2}         - \n`; 
+
+                    } else {
+                        var arrivalTime = Math.floor((Math.random() * 10) + 1);
                         arrivalResponse += `${bus.name2}         ${arrivalTime} min\n`;
-                    };
-                });
-                
-                ctx.reply(arrivalResponse, 
-                    Markup.inlineKeyboard([
-                        Markup.callbackButton('I don\'t know what bus to take. Help me!', `route:${source}`)
-                    ]).extra());
-                
-                db.close();
+                    }
+                };
+                ctx.reply(arrivalResponse, Markup.inlineKeyboard([
+                    Markup.callbackButton('I don\'t know what bus to take. Help me!', `route:${source}`)
+                    ]).extra()
+                );
             });
         });
 
